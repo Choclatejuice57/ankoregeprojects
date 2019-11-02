070d471b204e6ce7e0e32219d45a6ba3452ff8a3
Choclatejuice57 <chippadanikhil@gmail.com>
2019-11-02 12:32:13 -0700

    Ninja Fight

---
+++ Ninja_Fight.py
@@ -0,0 +1,8 @@
+ninjas = int(input("Enter Amount Of Ninjas"))
+if ninjas < 10:
+    print("I can fight those ninjas!")
+elif ninjas < 30:
+    print("It'll be a struggle, but i can fight em!")
+else:
+    print("That's too many!")
+
