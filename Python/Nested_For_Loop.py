a76ea8e70a970bd0b4c5a24f64d03558711a8d5a
Choclatejuice57 <56210566+Choclatejuice57@users.noreply.github.com>
2020-01-11 12:24:24 -0800

    Nestedforloop

---
+++ Python/Nested_For_Loop.py
@@ -0,0 +1,7 @@
+hugehairypants = ["huge","hairy",'pants']
+hhpfharryp = ["huge", "hairy","pants","for","harry", "potter"]
+for i in hugehairypants:
+    print('i is printing', i)
+    for j in hhpfharryp:
+        print("j is printing", j)
+
