124b2236540f9fe82b7aebd713f09cb19d284372
Choclatejuice57 <nikhilchippada57@gmail.com>
2020-03-22 14:14:45 -0700

    V-Buck Calculator. To be 100% Safe Download Python Before running.

--- Python/V-Bucks_In_A_Year.py
+++ Python/V-Bucks_In_A_Year.py
@@ -5,6 +5,13 @@ level_upgrades_per_vbucksReward = 6
 levelsxp_perDay = int(input("Welcome to the XP to V-Bucks Calculator! Enter XP Per Day"))
 #Initial XP
 xp = 0
-for week in range(1, 11):
+for week in range(1, 55):
+    # The part where your XP is multiplied by 7, then is added to your xp and printed.
     xp = levelsxp_perDay * 7 + xp
-    print('Week %s = Earned %s XP Till now' % (week, xp))
+    print('Week %s You Earned %s XP Till now' % (week, xp))
+    level_upgrades = int(xp / xp_per_level_upgrade)
+    print("   %s Level Upgrades" % (level_upgrades))
+    vBuck_reward = int(level_upgrades / level_upgrades_per_vbucksReward)
+    print('   %s Current V-Bucks Rewards' % (vBuck_reward))
+    vbucks = int(vBuck_reward * 100)
+    print("  %s Current V-Bucks" % (vbucks))
