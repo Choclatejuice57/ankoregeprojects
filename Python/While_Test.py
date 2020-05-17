6b2f67fc8a7aba79f78791089214b63de67ffdd4
Choclatejuice57 <nikhilchippada57@gmail.com>
2020-05-17 11:05:24 -0700

    While test for while loops, have at least 8 gigs of ram , I have 16 Oh yeah. Never eat that much food at a airport.

--- Python/While_Test.py
+++ Python/While_Test.py
@@ -1,11 +1,12 @@
-x = 45
-y = 80
-z = 700
+x = 50
+y = 50
+z = 999999999999999999999999999999999999999999999999999999999999999999999999999999999
 n = 0
-while x < 50 or y < 50 or z < 999999999:
+while x == 50 and y == 50 or z == 9999999999999999:
    # my pc will explode as it only has 1 byte of ram
     x = x + 1000000
     y = y + 1000000
     z = z + 1000000
     n = n + 1
     print(x, y, z, n)
+print("Total number of iterations =", n)
