6e3bc75c17b4bebe6073e2fc1434ccd790b49eab
Choclatejuice57 <nikhilchippada57@gmail.com>
2020-07-26 12:00:49 -0700

    Changelog: My 5 Favorite Ingredients is finished

---
+++ Python/MFFI.py
@@ -0,0 +1,6 @@
+ingredients = ['snails', 'leeches', 'gorilla belly-button lint', 'caterpillar eyebrows', 'centipede toes']
+counter = 0
+for a in ingredients:
+    counter = counter + 1
+    print(counter, a)
+
