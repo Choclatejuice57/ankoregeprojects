8aee6dac9e8b0da262e8f92b612362892e57c7e3
Choclatejuice57 <nikhilchippada57@gmail.com>
2020-09-13 11:27:57 -0700

    Function Test 1

--- Python/Function_Test.py
+++ Python/Function_Test.py
@@ -1,6 +1,12 @@
 def minecraft_inventory(item1):
     print('Select %s' %item1 )
 
+def minecraft_crafting(ct_wood):
+    tables = int(ct_wood / 4)
+    remaining_wood = ct_wood % 4
+    return tables, remaining_wood
 
-minecraft_inventory('Dirt')
-minecraft_inventory('Gold')
+minecraft_inventory('Wood')
+ca_tables, wood  = minecraft_crafting(7)
+print("Crafting Tables",ca_tables)
+print("Wood Left Over", wood)
